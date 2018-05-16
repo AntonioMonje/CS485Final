a4b1a786e0ab5882a6d5f4dc7f748583d6746427
Antonio Monje <monje001@cougars.csusm.edu>
2018-05-16 02:01:59 -0700

    finished final

---
+++ Assets/Scripts/Fire.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour {
+	private Rigidbody2D rb2d;
+	public float speed;
+	public float lifetime;
+	public int hp;
+	public PlayerController player;
+	public int death = 2;
+	// Use this for initialization
+	void Start () {
+		hp = 0;
+		rb2d = GetComponent<Rigidbody2D> ();	
+		player = FindObjectOfType<PlayerController> ();
+		if (player.transform.localScale.x < 0) {
+			speed = -speed;
+		}
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		rb2d.velocity = new Vector2 (speed, rb2d.velocity.y);	
+		Updatehp ();
+	}
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Enemy") {
+			Destroy (other.gameObject);
+		} 
+	
+		if (other.tag == "Boss") {
+			Debug.Log (hp);
+			Updatehp ();
+			if (hp <= death ) {
+				Destroy (other.gameObject);
+				youWin ();
+			} 
+			else 
+			{
+				return;
+			}
+		} 
+		if(other.tag != "Enemy" || other.tag != "Boss")
+		{
+			Destroy (gameObject, lifetime);
+		}
+	}
+	void Updatehp()
+	{
+		hp++;
+	}
+	void youWin()
+	{
+		Application.LoadLevel (4);
+	}
+}
---
+++ Assets/Scripts/enemyMover.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyMover : MonoBehaviour {
+	public float moveSpeed = 1;
+	public bool moveRight;
+
+	private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+		
+
+		rb2d = GetComponent<Rigidbody2D> ();
+
+	}
+	void Update(){
+		InvokeRepeating("movement", 0.0f, 0.5f);	
+	}
+	// Update is called once per frame
+	void movement() {
+		moveSpeed = -moveSpeed;
+
+			//rb2d.velocity = new Vector2 (-moveSpeed, rb2d.velocity.y);
+
+	}
+}
